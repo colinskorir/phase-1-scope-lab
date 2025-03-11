c88adc424bdf3d2d53031abc37ea01a4651d49ef
colinskorir <korircoll20@gmail.com>
2025-03-11 12:06:59 +0300

    files added

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
-// Write your solution in this file!
+var customerName = 'bob';
+
+function getCustomerName() {
+    return customerName;
+}
+
+function upperCaseCustomerName() {
+    customerName = customerName.toUpperCase();
+}
+
+var bestCustomer;
+
+function setBestCustomer() {
+    bestCustomer = 'not bob';
+}
+
+function overwriteBestCustomer() {
+    bestCustomer = 'maybe bob';
+}
+
+const leastFavoriteCustomer = 'someone';
+
+function changeLeastFavoriteCustomer() {
+    throw new Error('Assignment to constant variable.');
+}
